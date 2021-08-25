bfb1f831ec9ed31595bf1c70d03e86d2a8b7c5d5
Alexander Galagutskiy <alexander.galagutskiy@pm.me>
2021-08-25 16:54:35 +0300

    Updated to 2021.2.1

---
+++ src/main/scala/icons/TarantoolIcons.java
@@ -0,0 +1,18 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+import java.util.Objects;
+
+public interface TarantoolIcons {
+
+    Icon Tarantool13 = load("tarantool-icon-13.svg");
+
+    static Icon load(String path) {
+        Objects.requireNonNull(path);
+        Icon icon = IconLoader.getIcon("/icons/" + path);
+        return Objects.requireNonNull(icon);
+    }
+
+}
