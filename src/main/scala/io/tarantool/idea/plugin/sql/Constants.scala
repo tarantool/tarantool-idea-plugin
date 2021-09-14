4cd8506706a16338068102e1094f17230e81efb9
Alexander Galagutskiy <alexander.galagutskiy@pm.me>
2021-09-14 20:18:26 +0300

    GH-6: Fixed problem with DBMS registration

--- src/main/scala/io/tarantool/idea/plugin/sql/Constants.scala
+++ src/main/scala/io/tarantool/idea/plugin/sql/Constants.scala
@@ -4,5 +4,5 @@ import scala.language.postfixOps
 
 object Constants {
   val tarantool: String     = "Tarantool"
-  val dbmsTarantool: String = tarantool
+  val dbmsTarantool: String = "TARANTOOL"
 }
--- src/main/scala/io/tarantool/idea/plugin/sql/DbmsHolder.java
+++ src/main/scala/io/tarantool/idea/plugin/sql/DbmsHolder.java
@@ -1,29 +1,9 @@
 package io.tarantool.idea.plugin.sql;
 
 import com.intellij.database.Dbms;
-import com.intellij.util.ConcurrencyUtil;
 import icons.TarantoolIcons;
+import static io.tarantool.idea.plugin.sql.Constants.*;
 
-import java.util.concurrent.ConcurrentMap;
-
-@SuppressWarnings("unchecked")
 public final class DbmsHolder {
-
-    public static final Dbms DBMS;
-
-    static {
-        try {
-            DBMS = Dbms.create(Constants.tarantool(), TarantoolIcons.Tarantool13);
-            final var ourValuesField = Dbms.class.getDeclaredField("ourValues");
-            ourValuesField.setAccessible(true);
-            final var ourValues = (ConcurrentMap<String, Dbms>) ourValuesField.get(null);
-            ConcurrencyUtil.cacheOrGet(ourValues, Constants.dbmsTarantool(), DBMS);
-        } catch (NoSuchFieldException | IllegalAccessException ex) {
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
-    private DbmsHolder() {
-        throw new AssertionError();
-    }
+    public static final Dbms DBMS = Dbms.create(dbmsTarantool(), tarantool(), TarantoolIcons.Tarantool13);
 }
