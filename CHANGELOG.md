160e5e96e0a8775269a7348ef79a2b33e2bd2692
Roman Kvasnytskyi <culidesta@gmail.com>
2021-11-03 18:39:02 +0200

    Support Intellij 2021.3 Platform

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,16 @@
 # Changelog
 
 ## [Unreleased]
+### Breaking Changes
+- Updated minimal Intellij Platform version to 2021.3
+
 ### Added
 - Added [plugin signing](https://plugins.jetbrains.com/docs/intellij/plugin-signing.html) functionality
 
 ### Changed
-- Updated _org.jetbrains.intellij_ plugin from 1.1.4 to 1.1.6
-- Updated _org.jetbrains.changelog_ plugin from 1.2.1 to 1.3.0
+- Updated _org.jetbrains.intellij_ plugin from 1.1.4 to 1.2.1
+- Updated _org.jetbrains.changelog_ plugin from 1.2.1 to 1.3.1
+- Moved to Scala 3
 - Removed usage of Reflection API in DBMS registration
 
 ## [0.0.1] - 2021-08-13
--- src/main/scala/io/tarantool/idea/plugin/sql/DbmsHolder.java
+++ src/main/scala/io/tarantool/idea/plugin/sql/DbmsHolder.java
@@ -2,8 +2,41 @@ package io.tarantool.idea.plugin.sql;
 
 import com.intellij.database.Dbms;
 import icons.TarantoolIcons;
-import static io.tarantool.idea.plugin.sql.Constants.*;
+
+import javax.swing.*;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.util.function.Supplier;
+
+import static io.tarantool.idea.plugin.sql.Constants.dbmsTarantool;
+import static io.tarantool.idea.plugin.sql.Constants.tarantool;
 
 public final class DbmsHolder {
-    public static final Dbms DBMS = Dbms.create(dbmsTarantool(), tarantool(), TarantoolIcons.Tarantool13);
+
+    public static final Dbms DBMS;
+
+    static {
+        //TODO: Remove this backward compatibility trick after Intellij 2021.3 Release
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+        try {
+            Dbms dbms;
+            try {
+                final var types = MethodType.methodType(Dbms.class, String.class, String.class, Icon.class);
+                final var mh = lookup.findStatic(Dbms.class, "create", types);
+                dbms = (Dbms) mh.invokeExact(dbmsTarantool(), tarantool(), TarantoolIcons.Tarantool13);
+            } catch (final NoSuchMethodException e) {
+                final Supplier<Icon> createIcon = () -> TarantoolIcons.Tarantool13;
+                final var types = MethodType.methodType(Dbms.class, String.class, String.class, Supplier.class);
+                final var mh = lookup.findStatic(Dbms.class, "create", types);
+                dbms = (Dbms) mh.invokeExact(dbmsTarantool(), tarantool(), createIcon);
+            }
+            DBMS = dbms;
+        } catch (final Throwable e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    private DbmsHolder() {
+        throw new AssertionError("Suppress default constructor for noninstantiability");
+    }
 }
